e0c4c916cab7577474d158d73d8483e0272d9cb0
n0pr0blem <k.bobkov1408@gmail.com>
2025-07-24 17:22:07 +0300

    new: README.md, V2 migration and add postman collection

--- auth-service/src/main/resources/db/migration/V2__INSERT_DATA.sql
+++ auth-service/src/main/resources/db/migration/V2__INSERT_DATA.sql
@@ -1,2 +1,3 @@
 INSERT INTO users (username,password,email,role,is_active,first_name,last_name) VALUES
-('NoProblem','nQRw9l7IN/9SVtsFv/alf2bNVJHvJmprBOA14iNp8WI=','k.bobkov1408@gmail.com','ADMIN',true,'Konstantin','Bobkov');
+('Admin','DAcV0mdOQ/XMB3/ZzfPQXn3rQPu22pykEs4JSNLxwMY=','k.bobkov1408@gmail.com','ADMIN',true,'Konstantin','Bobkov'),
+('User','DAcV0mdOQ/XMB3/ZzfPQXn3rQPu22pykEs4JSNLxwMY=','user@gmail.com','USER',true,'Konstantin','Bobkov');
