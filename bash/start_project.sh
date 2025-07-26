be45cf3e826239e83091229db26be990887af3d9
n0pr0blem <k.bobkov1408@gmail.com>
2025-07-26 13:37:32 +0300

    new: update Dockerfiles, docker-compose.yaml and start_project.sh

--- bash/start_project.sh
+++ bash/start_project.sh
@@ -8,9 +8,6 @@ cd ..
 echo "Stoping containers..."
 sudo docker-compose stop
 
-echo "Starting Maven build..."
-mvn clean install -T 1C
-
 echo "Starting containers..."
 sudo docker-compose up --build -d
 
